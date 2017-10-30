7f505e1d98cc157d63bd361a9d521613c714ea17
Alex Lukashevich <aleksandr.dwt@gmail.com>
2017-10-30 21:26:58 +0300

    Added import bookmakers teams from file

--- app/server.js
+++ app/server.js
@@ -5,6 +5,7 @@
  */
 
 'use strict';
+const fs = require('fs');
 
 const parser = require("./parser");
 const fetcher = require("./fetcher");
@@ -21,189 +22,44 @@ const mongoClient = require('mongodb').MongoClient;
 const mongoClientUrl = 'mongodb://mongodb:27017/scoresdb';
 let mongoDB;
 
-const app = express();
-// app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(express.static('web'));
-
-/**
- * Access Control
- */
-app.use(function(req, res, next) {
-    if (typeof req.query.password !== 'undefined' &&  req.query.password === PASSWORD) {
-        next();
-    } else {
-        res.status(403).send();
-    }
-});
-
-/**
- * Date period parameters checker
- */
-app.use(function(req, res, next) {
-    let isValid = true;
-    for (const type of ['dateTill', 'dateFrom']) {
-        if (typeof req.query[type] === 'undefined' || req.query[type] === '') {
-            isValid = false;
-            break;
-        }
-    }
-
-    if (isValid) {
-        next();
-    } else {
-        res.status(422).send();
-    }
-});
-
-/**
- * List of tournaments
- */
-app.get('/tournaments', (req, res, next) =>
-    fetcher
-        .getTournamentsList(!!req.query.isArchived)
-        .then((result) => res.json(result))
-);
-
-/**
- * List of teams in tournament
- */
-app.get('/teams/:tournamentId', (req, res, next) =>
-    fetcher
-        .getTeamsList(req.params.tournamentId)
-        .then((result) => res.json(result))
-);
-
-/**
- * Score table of tournament
- */
-app.get('/score-table/:tournamentId', (req, res, next) =>
-    fetcher
-        .getTournamentResults(
-            req.params.tournamentId,
-            {dateFrom: req.query.dateFrom, dateTill: req.query.dateTill}
-        )
-        .then((result) => res.json(result))
-);
-
-/**
- * All matches of tournament
- */
-app.get('/all-matches-table/:tournamentId', (req, res, next) =>
-    fetcher
-        .getTournamentMatches(
-            req.params.tournamentId,
-            {dateFrom: req.query.dateFrom, dateTill: req.query.dateTill},
-            true
-        )
-        .then((result) => res.json(result))
-);
-
-/**
- * All matches of one team in tournament
- */
-app.get('/team-matches-table/:tournamentId/:teamId', (req, res, next) =>
-    fetcher
-        .getTeamMatches(
-            req.params.tournamentId,
-            req.params.teamId,
-            {dateFrom: req.query.dateFrom, dateTill: req.query.dateTill},
-            true
-        )
-        .then((result) => res.json(result))
-);
-
-/**
- * Forecast
- */
-app.get('/forecast/:num', (req, res, next) =>
-    fetcher
-        .getForecast(req.params.num, req.query)
-        .then((result) => res.json(result))
-);
-
-/**
- * Get site url
- */
-app.get('/site_urls/:tournamentId', (req, res, next) => {
-    let id = `${req.params.tournamentId}`;
-    let info = URLS.urls.find(o => o.id == id);
-    let officesUrls = info
-        ? info.urls
-        : ['/', '/', '/'];
-
-    res.json({
-        matchesUrl: createUrl(id, true),
-        scoreTableUrl: createUrl(id),
-        officesUrls: officesUrls.map((val, index) => eval(`OFFICE${index}_SITE`) + val)
-    });
-});
-
-/**
- * Forecast for next matches
- */
-app.get('/next-matches', (req, res, next) =>
-    fetcher.getNextMatches().then((result) => res.json(result))
-);
-
-/**
- * Mark future game as favorite
- */
-app.put('/favorite_game', (req, res, next) =>
-    fetcher.markGameAsFavorite(req.body.id).then(() => res.json({}))
-);
-
-/**
- * Unmark future game as favorite
- */
-app.delete('/favorite_game', (req, res, next) =>
-    fetcher.markGameAsFavorite(req.body.id, false).then(() => res.json({}))
-);
-
 function connectDB() {
     mongoClient
         .connect(mongoClientUrl)
         .then((db) => {
-            fetcher.setMongoDb(db);
-            mongoDB = db;
+            let collection = db.collection('bookmakers_teams');
+            let contents = fs.readFileSync('./1.json', 'utf8');
+            contents = JSON.parse(contents);
 
-            // start parsing sites forever
-            parser.start(
-                URLS.archive.map((o) => ({
-                    id: o.id,
-                    url: createUrl(o.id, true),
-                    isArchive: true,
-                }))
-                .concat(
-                    URLS.urls.map((o) => ({
-                        id: o.id,
-                        url: createUrl(o.id, true)
-                    }))
-                ),
-                mongoDB
-            );
-            
-            app.listen(80);
-        }).catch((err) =>  {
-            console.log('Trying connecting MongoDB...');
-            setTimeout(connectDB, 1000);
-        });
+            for (const item of contents) {
+                collection.insertOne({
+                    bookmakerId: 2,
+                    tournamentId: item.tournamentId,
+                    teamId: item.teamId,
+                    teamName: item.teamName,
+                }).catch(() => { });
+            }
+
+
+            // fs.readdirSync('./teams/').every(file => {
+            //     file = './teams/' + file;
+
+                // let contents = fs.readFileSync(file, 'utf8');
+                // contents = JSON.parse(contents);
+
+                // for (let j = 0; j < contents.teamsToDo.length; j++) {
+                //     let pos = contents.teamsToDo[j].value.indexOf('@');
+                //     // if (pos !== -1) {
+                //         collection.insertOne({
+                //             bookmakerId: 2,
+                //             tournamentId: contents.id,
+                //             teamId: parseInt(contents.teamsToDo[j].value.substring(pos + 1)),
+                //             teamName: trim(contents.teamsToDo[j].name.toLowerCase()),
+                //         }).catch(() => { });
+
+
+
+                    });
 }
 
-function createUrl(tournamentId, isMatchesUrl) {
-    let result = `${SITE_URL}/?sport=soccer&id=${parseInt(tournamentId.replace('a', ''))}&page=`;
-    if (tournamentId.indexOf('r') !== -1) {
-        result += 'round';
-    } else if (tournamentId.indexOf('s') !== -1) {
-        result += 'season';
-    } else {
-        result += 'competition';
-    }
-    if (isMatchesUrl) {
-        result += '&view=matches';
-    }
-
-    return result;
-}
 
 connectDB();
