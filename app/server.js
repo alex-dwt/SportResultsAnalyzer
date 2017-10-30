84756f146a05a6b7df69e500558a6499312ac450
Alex Lukashevich <alex.lukashevich@intellectsoft.net>
2017-10-30 10:17:19 +0300

    Added writing bookmakers teams to db

--- app/server.js
+++ app/server.js
@@ -17,168 +17,21 @@ const OFFICE2_SITE = process.env.OFFICE3_SITE;
 const PASSWORD = process.env.PASSWORD;
 const URLS = require("./sites");
 
-const parserB = require("./parser-bookmakers");
+const parserB = require("./write-bookmakers-to-db");
 
 
 const mongoClient = require('mongodb').MongoClient;
 const mongoClientUrl = 'mongodb://mongodb:27017/scoresdb';
 let mongoDB;
 
-const app = express();
-// app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(express.static('web'));
 
-/**
- * Access Control
- */
-app.use(function(req, res, next) {
-    if (typeof req.query.password !== 'undefined' &&  req.query.password === PASSWORD) {
-        next();
-    } else {
-        res.status(403).send();
-    }
-});
-
-/**
- * Date period parameters checker
- */
-app.use(function(req, res, next) {
-    let isValid = true;
-    for (const type of ['dateTill', 'dateFrom']) {
-        if (typeof req.query[type] === 'undefined' || req.query[type] === '') {
-            isValid = false;
-            break;
-        }
-    }
-
-    if (isValid) {
-        next();
-    } else {
-        res.status(422).send();
-    }
-});
-
-/**
- * List of tournaments
- */
-app.get('/tournaments', (req, res, next) =>
-    fetcher
-        .getTournamentsList(!!req.query.isArchived)
-        .then((result) => res.json(result))
-);
-
-/**
- * List of teams in tournament
- */
-app.get('/teams/:tournamentId', (req, res, next) =>
-    fetcher
-        .getTeamsList(req.params.tournamentId)
-        .then((result) => res.json(result))
-);
-
-/**
- * Score table of tournament
- */
-app.get('/score-table/:tournamentId', (req, res, next) =>
-    fetcher
-        .getTournamentResults(
-            req.params.tournamentId,
-            {dateFrom: req.query.dateFrom, dateTill: req.query.dateTill}
-        )
-        .then((result) => res.json(result))
-);
-
-/**
- * All matches of tournament
- */
-app.get('/all-matches-table/:tournamentId', (req, res, next) =>
-    fetcher
-        .getTournamentMatches(
-            req.params.tournamentId,
-            {dateFrom: req.query.dateFrom, dateTill: req.query.dateTill},
-            true
-        )
-        .then((result) => res.json(result))
-);
-
-/**
- * All matches of one team in tournament
- */
-app.get('/team-matches-table/:tournamentId/:teamId', (req, res, next) =>
-    fetcher
-        .getTeamMatches(
-            req.params.tournamentId,
-            req.params.teamId,
-            {dateFrom: req.query.dateFrom, dateTill: req.query.dateTill},
-            true
-        )
-        .then((result) => res.json(result))
-);
-
-/**
- * Forecast
- */
-app.get('/forecast/:num', (req, res, next) =>
-    fetcher
-        .getForecast(req.params.num, req.query)
-        .then((result) => res.json(result))
-);
-
-/**
- * Get site url
- */
-app.get('/site_urls/:tournamentId', (req, res, next) => {
-    let id = `${req.params.tournamentId}`;
-    let info = URLS.urls.find(o => o.id == id);
-    let officesUrls = info
-        ? info.urls
-        : ['/', '/', '/'];
-
-    res.json({
-        matchesUrl: createUrl(id, true),
-        scoreTableUrl: createUrl(id),
-        officesUrls: officesUrls.map((val, index) => eval(`OFFICE${index}_SITE`) + val)
+mongoClient
+    .connect(mongoClientUrl)
+    .then((db) => {
+        mongoDB = db;
+        parserB.start(
+            mongoDB
+        );
     });
-});
-
-/**
- * Forecast for next matches
- */
-app.get('/next-matches', (req, res, next) =>
-    fetcher.getNextMatches().then((result) => res.json(result))
-);
-
-/**
- * Mark future game as favorite
- */
-app.put('/favorite_game', (req, res, next) =>
-    fetcher.markGameAsFavorite(req.body.id).then(() => res.json({}))
-);
-
-/**
- * Unmark future game as favorite
- */
-app.delete('/favorite_game', (req, res, next) =>
-    fetcher.markGameAsFavorite(req.body.id, false).then(() => res.json({}))
-);
-
-function createUrl(tournamentId, isMatchesUrl) {
-    let result = `${SITE_URL}/?sport=soccer&id=${parseInt(tournamentId.replace('a', ''))}&page=`;
-    if (tournamentId.indexOf('r') !== -1) {
-        result += 'round';
-    } else if (tournamentId.indexOf('s') !== -1) {
-        result += 'season';
-    } else {
-        result += 'competition';
-    }
-    if (isMatchesUrl) {
-        result += '&view=matches';
-    }
-
-    return result;
-}
 
 
-
-parserB.start();
---
+++ app/write-bookmakers-to-db.js
@@ -0,0 +1,63 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const fetcher = require("./fetcher");
+const fs = require('fs');
+
+let bookmakersCollection,
+    isStarted,
+    urls, collection;
+
+function trim(string) {
+    return string.replace(/^\s*|\s*$/g, '');
+}
+
+module.exports = {
+    start(mongoDb) {
+        collection = mongoDb.collection('bookmakers_teams');
+
+
+
+        fs.readdirSync('./teams/').every(file => {
+            file = './teams/' + file;
+
+            let contents = fs.readFileSync(file, 'utf8');
+            contents = JSON.parse(contents);
+
+            for (let j = 0; j < contents.teamsToDo.length; j++) {
+                let pos = contents.teamsToDo[j].value.indexOf('@');
+                if (pos !== -1) {
+                    collection.insertOne({
+                        bookmakerId: 2,
+                        tournamentId: contents.id,
+                        teamId: parseInt(contents.teamsToDo[j].value.substring(pos + 1)),
+                        teamName: trim(contents.teamsToDo[j].name.toLowerCase()),
+                    }).catch(() => { });
+
+                    contents.teamsToDo[j].name = '';
+                }
+            }
+
+            contents.teamsToDo = contents.teamsToDo.filter(o => o.name !== '');
+
+            fs.writeFile(file, JSON.stringify(contents), function(err) {
+                console.log('Done ' + file);
+            });
+
+            return true;
+        });
+
+    }
+};
+
+
+
+
+function sortStringsAlphabetically(a, b, field) {
+    if (a < b) {
+        return -1;
+    }
+    if (a > b) {
+        return 1;
+    }
+    return 0;
+}
