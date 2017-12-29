c77d135d4754be1338563a1eff144b3ec04fe338
Alex Lukashevich <alex.lukashevich@intellectsoft.net>
2017-12-29 11:15:49 +0300

    Improved filters

--- app/front/app/filterBlocks/AnalysisTableFilterBlock.jsx
+++ app/front/app/filterBlocks/AnalysisTableFilterBlock.jsx
@@ -6,58 +6,44 @@ import The5thForecastAllPlusesFilter  from '../filters/The5thForecastAllPlusesFi
 const includes = require('array-includes');
 const intersect = require('intersect');
 import NextTabFilterBlock  from '../filterBlocks/NextTabFilterBlock.jsx'
+import StrongerTeamFilter  from '../filters/StrongerTeamFilter.jsx'
+import PositionsDiffMinFilter  from '../filters/PositionsDiffMinFilter.jsx'
 
 const NO_MINUSES_FILTER_ID = 0;
 const ALL_PLUSES_FILTER_ID = 1;
+const STRONGER_TEAM_FILTER_ID = 2;
+const POSITIONS_MIN_DIFF_FILTER_ID = 3;
 
 export default class extends NextTabFilterBlock {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...this.state,
-            errorMessage: ''
-        };
-    }
-
     handleFilterClick() {
-        this.setState({errorMessage:''});
-
         let positiveFilteredItemsIds = [];
         let negativeFilteredItemsIds = [];
         let neutralFilteredItemsIds = [];
 
-        if (Object.keys(this.filteredValues).length) {
-            if (this.filteredValues.hasOwnProperty(NO_MINUSES_FILTER_ID)
-                && this.filteredValues.hasOwnProperty(ALL_PLUSES_FILTER_ID)
-            ) {
-                this.setState({
-                    errorMessage: 'You can not use filters "5 forecast no "-"" and "5 forecast all "+"" together'
-                });
-
-                return;
+        if (this.filteredValues.hasOwnProperty(NO_MINUSES_FILTER_ID)
+            || this.filteredValues.hasOwnProperty(ALL_PLUSES_FILTER_ID)
+        ) {
+            let idsArr = [];
+            for (const val of Object.values(this.filteredValues)) {
+                idsArr.push(val.map((o) => o._id));
             }
+            for (const id of intersect(idsArr)) {
+                let item = this.props.items.find((o) => o._id === id);
+                if (item) {
+                    let strongerTeam = item
+                        .extraInfo
+                        .scores.find(o => o.forecastNum === 5)
+                        .value.find(o => o.info.type === 'forecast1').info.strongerTeam;
 
-            let items = [];
-            if (this.filteredValues.hasOwnProperty(NO_MINUSES_FILTER_ID)) {
-                items = this.filteredValues[NO_MINUSES_FILTER_ID];
-            } else if (this.filteredValues.hasOwnProperty(ALL_PLUSES_FILTER_ID)) {
-                items = this.filteredValues[ALL_PLUSES_FILTER_ID];
-            }
-
-            for (const item of items) {
-                let strongerTeam = item
-                    .extraInfo
-                    .scores.find(o => o.forecastNum === 5)
-                    .value.find(o => o.info.type === 'forecast1').info.strongerTeam;
-
-                if ((strongerTeam === 'home' && item.homeScore > item.guestScore) ||
-                    (strongerTeam === 'guest' && item.homeScore < item.guestScore)
-                ) {
-                    positiveFilteredItemsIds.push(item._id);
-                } else if (item.homeScore === item.guestScore) {
-                    neutralFilteredItemsIds.push(item._id);
-                } else {
-                    negativeFilteredItemsIds.push(item._id);
+                    if ((strongerTeam === 'home' && item.homeScore > item.guestScore) ||
+                        (strongerTeam === 'guest' && item.homeScore < item.guestScore)
+                    ) {
+                        positiveFilteredItemsIds.push(item._id);
+                    } else if (item.homeScore === item.guestScore) {
+                        neutralFilteredItemsIds.push(item._id);
+                    } else {
+                        negativeFilteredItemsIds.push(item._id);
+                    }
                 }
             }
         }
@@ -74,6 +60,24 @@ export default class extends NextTabFilterBlock {
             <div className="analysis-table-filter-block">
 
                 <Grid divided>
+
+                    <Grid.Column>
+                        <StrongerTeamFilter
+                            index={STRONGER_TEAM_FILTER_ID}
+                            items={this.props.items}
+                            onChange={this.handleFilterSelect.bind(this)}
+                            payloadCallback={(item) => item.extraInfo.positions.guestThen - item.extraInfo.positions.homeThen}
+                        />
+                    </Grid.Column>
+
+                    <Grid.Column>
+                        <PositionsDiffMinFilter
+                            index={POSITIONS_MIN_DIFF_FILTER_ID}
+                            items={this.props.items}
+                            onChange={this.handleFilterSelect.bind(this)}
+                            payloadCallback={(item) => Math.abs(item.extraInfo.positions.homeThen - item.extraInfo.positions.guestThen)}
+                        />
+                    </Grid.Column>
 
                     <Grid.Column>
                         <The5thForecastNoMinusesFilter
@@ -94,8 +98,6 @@ export default class extends NextTabFilterBlock {
                     </Grid.Column>
 
                 </Grid>
-
-                <p style={{color: 'red', marginTop: '10px'}}>{this.state.errorMessage}</p>
 
                 <Button  style={{marginTop: '10px'}} onClick={(e) => this.handleFilterClick(e)}>
                     Filter
